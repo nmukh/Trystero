b0630315d9ab18fe91903f486e2591390cec8543
Nishat Mukherji <nmukh1@gmail.com>
2020-08-14 13:53:12 -0500

    list contacts method

--- server/src/Contacts.ts
+++ server/src/Contacts.ts
@@ -7,4 +7,31 @@ export interface IContact {
     _id?: number,
     name: string,
     email: string
-  }
+}
+
+export class Worker {
+    private db: Nedb;
+    constructor() {
+        this.db = new Datastore({
+            filename: path.join(__dirname, "contacts.db"),
+            autoload: true
+        });
+    }
+
+    //Promise to resolve returning array of IContact items using generics
+    public listContacts(): Promise<IContact[]> {
+        return new Promise((inResolve, inReject) => {
+            //pass empty search criteria and return all records
+            this.db.find({},
+                (inError: Error, inDocs: IContact[]) => {
+                    if (inError) {
+                        inReject(inError);
+                    } else {
+                        inResolve(inDocs);
+                    }
+                }
+            );
+        });
+    }
+
+}
