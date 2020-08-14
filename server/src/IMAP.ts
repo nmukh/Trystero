3832293cb20582293a0836d455cd06ceb2608ab9
Nishat Mukherji <nmukh1@gmail.com>
2020-08-14 11:47:15 -0500

    imap worker class with connect to server

--- server/src/IMAP.ts
+++ server/src/IMAP.ts
@@ -7,23 +7,51 @@ import { IServerInfo } from "./ServerInfo";
 
 //Interface describing mailbox
 export interface ICallOptions {
-mailbox: string,
-id?: number
+    mailbox: string,
+    id?: number
 }
 
 //Interface describing received message
 export interface IMessage {
-id: string, date: string,
-from: string,
-subject: string, body?: string
+    id: string, date: string,
+    from: string,
+    subject: string, body?: string
 }
 
 //Interface describing mailbox
 export interface IMailbox {
-  name: string,
-  path: string
+    name: string,
+    path: string
 }
 
 // Disable certificate validation, necessary for making IMAP server calls 
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
 
+//Define worker class
+export class Worker {
+    private static serverInfo: IServerInfo;
+    constructor(inServerInfo: IServerInfo) {
+        Worker.serverInfo = inServerInfo;
+    }
+    //Creates emailjs-imap-client object and connects it to server
+    private async connectToServer(): Promise<any> {
+        //Create client with and host and port and user/password from serverInfo
+        const client: any = new ImapClient.default(
+            Worker.serverInfo.imap.host,
+            Worker.serverInfo.imap.port,
+            { auth: Worker.serverInfo.imap.auth }
+        );
+        //reduce logging information noise
+        client.logLevel = client.LOG_LEVEL_NONE;
+        //error handler
+        client.onerror = (inError: Error) => {
+            console.log(
+                "IMAP.Worker.listMailboxes(): Connection error",
+                inError
+            );
+        };
+        await client.connect();
+        //client returned to caller
+        return client;
+    }
+}
