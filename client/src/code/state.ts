2abe22e71cfd02881abb8918e91ce749bd294e9e
Nishat Mukherji <nmukh1@gmail.com>
2020-09-01 18:07:07 -0500

    add state properties

--- client/src/code/state.ts
+++ client/src/code/state.ts
@@ -7,7 +7,30 @@ import * as SMTP from "./SMTP";
 export function createState(inParentComponent) {
 
     return {
-        waitVisible : false
+        waitVisible: false,
+        contacts: [],
+
+        mailboxes: [],
+
+        messages: [],
+
+        // The view that is currently showing ("welcome", "message", "compose", "contact" or "contactAdd").
+        currentView: "welcome",
+
+        currentMailbox: null,
+
+        // The details of the message currently being viewed or composed 
+        messageID: null,
+        messageDate: null,
+        messageFrom: null,
+        messageTo: null,
+        messageSubject: null,
+        messageBody: null,
+
+        // The details of the contact currently being viewed or added
+        contactID: null,
+        contactName: null,
+        contactEmail: null
 
     }
 };
