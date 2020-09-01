bd4bcbabea96414d01dac96b99b6e56ad70d10fc
Nishat Mukherji <nmukh1@gmail.com>
2020-09-01 15:16:58 -0500

    app imports

--- client/src/code/main.tsx
+++ client/src/code/main.tsx
@@ -4,4 +4,8 @@ import "../css/main.css";
 import React from "react";
 import ReactDOM from "react-dom";
 
+import BaseLayout from "./components/BaseLayout";
+import * as IMAP from "./IMAP";
+import * as Contacts from "./Contacts";
+
 
