8d1630c9fd681ab87828c4961434fd245fe0e24a
Nishat Mukherji <nmukh1@gmail.com>
2020-09-03 17:49:53 -0500

    call getmailboxes

--- client/src/code/main.tsx
+++ client/src/code/main.tsx
@@ -13,13 +13,23 @@ const baseComponent = ReactDOM.render(<BaseLayout />, document.body);
 
 //Fetch mailbox and contacts
 baseComponent.state.showHidePleaseWait(true);
-
 async function getMailboxes() {
-    const imapWorker: IMAP.Worker = new IMAP.Worker();
-    const mailboxes: IMAP.IMailbox[] = await imapWorker.listMailboxes();
-    mailboxes.forEach((inMailbox) => {
-      baseComponent.state.addMailboxToList(inMailbox);
+  const imapWorker: IMAP.Worker = new IMAP.Worker();
+  const mailboxes: IMAP.IMailbox[] = await imapWorker.listMailboxes();
+  mailboxes.forEach((inMailbox) => {
+    baseComponent.state.addMailboxToList(inMailbox);
+  });
+}
+getMailboxes().then(function() {
+  // Now go fetch the user's contacts.
+  async function getContacts() {
+    const contactsWorker: Contacts.Worker = new Contacts.Worker();
+    const contacts: Contacts.IContact[] = await contactsWorker.listContacts();
+    contacts.forEach((inContact) => {
+      baseComponent.state.addContactToList(inContact);
     });
   }
-  
+  getContacts().then(() => baseComponent.state.showHidePleaseWait(false));
+});
+
 
