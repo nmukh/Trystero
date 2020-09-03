0e920c61335de9809f76463adf6bdc34755ede03
Nishat Mukherji <nmukh1@gmail.com>
2020-09-03 18:54:48 -0500

    return a list of all contacts from the server

--- client/src/code/Contacts.ts
+++ client/src/code/Contacts.ts
@@ -6,3 +6,11 @@ export interface IContact {
 _id?: number, name: string, email: string
 }
 
+export class Worker {
+    public async listContacts(): Promise<IContact[]> {
+        const response: AxiosResponse =
+        await axios.get(`${config.serverAddress}/contacts`);
+        return response.data;
+        }
+        
+}
