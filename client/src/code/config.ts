2e1a6b87b37fd5b8e783ee8ebb0fe9e7ce1d37a0
Nishat Mukherji <nmukh1@gmail.com>
2020-09-03 17:56:55 -0500

    config file to allow client app to talk with server

---
+++ client/src/code/config.ts
@@ -0,0 +1,6 @@
+export const config: { serverAddress: string, userEmail: string } =
+{
+  serverAddress : "http://localhost",
+//update userEmail with appropriate field
+  userEmail : "name@domain.com"
+};
