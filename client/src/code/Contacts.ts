fb4f5ad383b0f2e96d30579b7790de5ec770113b
Nishat Mukherji <nmukh1@gmail.com>
2020-09-03 19:59:06 -0500

    add a contact to the server

--- client/src/code/Contacts.ts
+++ client/src/code/Contacts.ts
@@ -3,14 +3,21 @@ import { config } from "./config";
 
 //interface describing contact. _id is optional
 export interface IContact {
-_id?: number, name: string, email: string
+    _id?: number, name: string, email: string
 }
 
 export class Worker {
     public async listContacts(): Promise<IContact[]> {
         const response: AxiosResponse =
-        await axios.get(`${config.serverAddress}/contacts`);
+            await axios.get(`${config.serverAddress}/contacts`);
         return response.data;
-        }
-        
+    }
+
+    public async addContact(inContact: IContact): Promise<IContact> {
+        const response: AxiosResponse = await axios.post(
+            `${config.serverAddress}/contacts`, inContact
+        );
+        return response.data;
+    }
+
 }
