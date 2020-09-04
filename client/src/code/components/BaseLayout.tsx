9509a5459cd3fe3418340e7672271c985b74d807
Nishat Mukherji <nmukh1@gmail.com>
2020-09-04 13:28:43 -0500

    contactList and associated style rule

--- client/src/code/components/BaseLayout.tsx
+++ client/src/code/components/BaseLayout.tsx
@@ -15,34 +15,49 @@ import ContactView from "./ContactView";
 import MessageView from "./MessageView";
 import { createState } from "../state";
 
-
-
 class BaseLayout extends Component {
+  state = createState(this);
 
-    state = createState(this);
-
-    render() {
-        return (
-            <div className="appContainer">
-                <Dialog open={this.state.pleaseWaitVisible} disableBackdropClick={true} disableEscapeKeyDown={true}
-                    transitionDuration={0}>
-                    <DialogTitle style={{ textAlign: "center" }}>Please Wait</DialogTitle>
-                    <DialogContent><DialogContentText>...Contacting server...</DialogContentText></DialogContent>
-                </Dialog>
-                <div className="toolbar"><Toolbar state={this.state} /></div>
-                <div className="mailboxList"><MailboxList state={this.state} /></div>
-                <div className="centerArea">
-                    <div className="messageList"><MessageList state={this.state} /></div>
-                    <div className="centerViews">
-                        {this.state.currentView === "welcome" && <WelcomeView />}
-                        {(this.state.currentView === "message" || this.state.currentView === "compose") &&
-                            <MessageView state={this.state} />
-                        }
-                        {(this.state.currentView === "contact" || this.state.currentView === "contactAdd") &&
-                            <ContactView state={this.state} />
-                        }
-                    </div>
-                </div>
-            </div>)
-    }
-};
+  render() {
+    return (
+      <div className="appContainer">
+        <Dialog
+          open={this.state.pleaseWaitVisible}
+          disableBackdropClick={true}
+          disableEscapeKeyDown={true}
+          transitionDuration={0}
+        >
+          <DialogTitle style={{ textAlign: "center" }}>Please Wait</DialogTitle>
+          <DialogContent>
+            <DialogContentText>...Contacting server...</DialogContentText>
+          </DialogContent>
+        </Dialog>
+        <div className="toolbar">
+          <Toolbar state={this.state} />
+        </div>
+        <div className="mailboxList">
+          <MailboxList state={this.state} />
+        </div>
+        <div className="centerArea">
+          <div className="messageList">
+            <MessageList state={this.state} />
+          </div>
+          <div className="centerViews">
+            {this.state.currentView === "welcome" && <WelcomeView />}
+            {(this.state.currentView === "message" ||
+              this.state.currentView === "compose") && (
+              <MessageView state={this.state} />
+            )}
+            {(this.state.currentView === "contact" ||
+              this.state.currentView === "contactAdd") && (
+              <ContactView state={this.state} />
+            )}
+          </div>
+        </div>
+        <div className="contactList">
+          <ContactList state={this.state} />
+        </div>
+      </div>
+    );
+  }
+}
