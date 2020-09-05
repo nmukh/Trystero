415128634b99fd3e8ea963e3691a7ac3866dd61c
Nishat Mukherji <nmukh1@gmail.com>
2020-09-05 18:33:43 -0500

    create contactList component

---
+++ client/src/code/components/ContactList.tsx
@@ -0,0 +1,9 @@
+//imports
+import React from "react";
+
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemAvatar from "@material-ui/core/ListItemAvatar";
+import Avatar from "@material-ui/core/Avatar";
+import Person from "@material-ui/icons/Person";
+import ListItemText from "@material-ui/core/ListItemText";
