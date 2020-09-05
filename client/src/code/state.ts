813df9b914067f0a38cc90553a3e068b891684fd
Nishat Mukherji <nmukh1@gmail.com>
2020-09-05 18:14:05 -0500

    show contactView in add mode and update tsconfig

--- client/src/code/state.ts
+++ client/src/code/state.ts
@@ -3,55 +3,98 @@ import { config } from "./config";
 import * as IMAP from "./IMAP";
 import * as SMTP from "./SMTP";
 
-
 export function createState(inParentComponent) {
+  return {
+    waitVisible: false,
+    contacts: [],
 
-    return {
-        waitVisible: false,
-        contacts: [],
+    mailboxes: [],
 
-        mailboxes: [],
+    messages: [],
 
-        messages: [],
+    // The view that is currently showing ("welcome", "message", "compose", "contact" or "contactAdd").
+    currentView: "welcome",
 
-        // The view that is currently showing ("welcome", "message", "compose", "contact" or "contactAdd").
-        currentView: "welcome",
+    currentMailbox: null,
 
-        currentMailbox: null,
+    // The details of the message currently being viewed or composed
+    messageID: null,
+    messageDate: null,
+    messageFrom: null,
+    messageTo: null,
+    messageSubject: null,
+    messageBody: null,
 
-        // The details of the message currently being viewed or composed 
-        messageID: null,
-        messageDate: null,
-        messageFrom: null,
-        messageTo: null,
-        messageSubject: null,
-        messageBody: null,
+    // The details of the contact currently being viewed or added
+    contactID: null,
+    contactName: null,
+    contactEmail: null,
 
-        // The details of the contact currently being viewed or added
+    showHidePleaseWait: function(inVisible: boolean): void {
+      this.setState({ pleaseWaitVisible: inVisible });
+    }.bind(inParentComponent),
+
+    addMailboxToList: function(inMailbox: IMAP.IMailbox): void {
+      //copy list
+      const cl: IMAP.IMailbox[] = this.state.mailboxes.slice(0);
+      //add new element
+      cl.push(inMailbox);
+      // update list in state
+      this.setState({ mailboxes: cl });
+    }.bind(inParentComponent),
+
+    addContactToList: function(inContact: Contacts.IContact): void {
+      const cl = this.state.contacts.slice(0);
+      cl.push({
+        _id: inContact._id,
+        name: inContact.name,
+        email: inContact.email,
+      });
+      this.setState({ contacts: cl });
+    }.bind(inParentComponent),
+
+    showComposeMessage: function(inType: string): void {
+      switch (inType) {
+        case "new":
+          this.setState({
+            currentView: "compose",
+            messageTo: "",
+            messageSubject: "",
+            messageBody: "",
+            messageFrom: config.userEmail,
+          });
+          break;
+
+        case "reply":
+          this.setState({
+            currentView: "compose",
+            messageTo: this.state.messageFrom,
+            messageSubject: `Re: ${this.state.messageSubject}`,
+            messageBody: `\n\n---- Original Message ----\n\n${this.state.messageBody}`,
+            messageFrom: config.userEmail,
+          });
+          break;
+
+        case "contact":
+          this.setState({
+            currentView: "compose",
+            messageTo: this.state.contactEmail,
+            messageSubject: "",
+            messageBody: "",
+            messageFrom: config.userEmail,
+          });
+          break;
+      }
+    }.bind(inParentComponent),
+    showAddContact: function(): void {
+      console.log("state.showAddContact()");
+
+      this.setState({
+        currentView: "contactAdd",
         contactID: null,
-        contactName: null,
-        contactEmail: null,
-
-
-        showHidePleaseWait: function (inVisible: boolean): void {
-            this.setState({ pleaseWaitVisible: inVisible });
-        }.bind(inParentComponent),
-
-        addMailboxToList: function (inMailbox: IMAP.IMailbox): void {
-            //copy list
-            const cl: IMAP.IMailbox[] = this.state.mailboxes.slice(0);
-            //add new element
-            cl.push(inMailbox);
-            // update list in state
-            this.setState({ mailboxes: cl });
-        }.bind(inParentComponent),
-
-        addContactToList : function(inContact: Contacts.IContact): void {
-            const cl = this.state.contacts.slice(0);
-            cl.push({ _id : inContact._id,
-            name : inContact.name, email : inContact.email });
-            this.setState({ contacts : cl });
-            }.bind(inParentComponent)
-
-    }
-};
+        contactName: "",
+        contactEmail: "",
+      });
+    }.bind(inParentComponent),
+  };
+}
